feb6838188986bf021f8d5b479aeccb4d2c203f8
takacsberni <takacsberni@gmail.com>
2021-11-16 13:51:00 +0100

    post an answer to push

--- server.py
+++ server.py
@@ -16,6 +16,26 @@ def list_questions():
     return render_template('list.html', questions=sorted_question_dict)
 #Hanna
 #Berni
+#new answer / post an answer
+@app.route('/question/<question_id>/new-answer', methods = ['GET', 'POST'])
+def new_answer(question_id):
+    from datetime import datetime
+    now = datetime.now()
+    now_timestamp = datetime.timestamp(now)
+    if request.method == "POST":
+        new_answer = {
+            "id": data_manager.get_max_id()+1, #A unique identifier for the answer.
+            "submission_time":int(now_timestamp), #floatot ad ki alapból, The UNIX timestamp when the answer is posted.
+            "vote_number":str(0), #The sum of votes the answer receives.
+            "question_id": 15,#Ide majd az az ID kell, ami ami a View Questionből jön, Verótól
+            "message": request.form.get("message"), #The answer text.
+            "image":request.form.get("image") #The path to the image for this answer.
+            }
+        data_manager.write_answers(new_answer)
+        return redirect("/question/"+str(question_id))
+    return render_template("new_answer.html", question_id = question_id)
+
+
 #Berni
 #Vero
 @app.route("/question/<question_id>")
