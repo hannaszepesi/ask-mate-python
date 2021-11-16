8fc87ed60fd0ad8981b80b6c0179dfea15d28b4e
takacsberni <takacsberni@gmail.com>
2021-11-16 15:59:53 +0100

    add answer works

--- data_manager.py
+++ data_manager.py
@@ -2,7 +2,7 @@ import csv
 import os
 dirname = os.path.dirname(__file__)
 QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-ANSWER_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message,image']
+ANSWER_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 ANSWER_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else dirname + '/sample_data/answer.csv'
 QUESTION_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else dirname + '/sample_data/question.csv'
 SORTING_OPTIONS = ['title', 'submission_time', 'message', 'view_number', 'vote_number']
@@ -35,8 +35,7 @@ def write_answers(answers):
     with open(ANSWER_PATH, 'a', newline='') as file:
         dictwriter_object = csv.DictWriter(file, fieldnames=ANSWER_HEADER)
         dictwriter_object.writeheader()
-        for answer in answers:
-            dictwriter_object.writerow(answer)
+        dictwriter_object.writerow(answers)
 
 
 def write_questions(questions):
