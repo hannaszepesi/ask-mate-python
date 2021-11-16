839382e0576ad04f1b54493a654718767398e360
takacsberni <takacsberni@gmail.com>
2021-11-16 10:35:27 +0100

    add_question route is done

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,5 @@
 import csv
 import os
-
 QUESTION_HEADER = ['id','submission_time','view_number','vote_number','title','message','image']
 ANSWER_HEADER = ['id','submission_time','vote_number','question_id','message,image']
 
