cbd19add0c946a8384a2c07f3dfa74f20c9dfdeb
Lutilaci <lutilaci@gmail.com>
2021-12-14 12:55:24 +0100

    import session and flash

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from werkzeug.utils import secure_filename
 import os
 from os import urandom
