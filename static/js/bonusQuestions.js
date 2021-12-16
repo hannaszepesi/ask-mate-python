3bffc8a22cf5995470120d6b9a7bd28594eb76fe
Hanna Szepesi <hannaszepesi.7@gmail.com>
2021-12-16 15:23:27 +0100

    Added filter questions all features working

--- static/js/bonusQuestions.js
+++ static/js/bonusQuestions.js
@@ -9,11 +9,14 @@ function getSortedItems(items, sortField, sortDirection) {
     // effect this function has on the table
     //
     if (sortDirection === "asc") {
-        const firstItem = items.shift()
-        if (firstItem) {
-            items.push(firstItem)
+        for (let i=0; i<items.length; i++) {
+            const firstItem = items.shift()
+            if (firstItem) {
+                items.push(firstItem)
+            }
         }
-    } else {
+    }
+    else {
         const lastItem = items.pop()
         if (lastItem) {
             items.push(lastItem)
@@ -33,13 +36,20 @@ function getFilteredItems(items, filterValue) {
     // effect this function has on the table
     //
     for (let i=0; i<items.length; i++) {
-        if (filterValue[0] === "!" && !(items[i]['Title'].includes(filterValue.substring(1, filterValue.length)) || items[i]['Description'].includes(filterValue.substring(1, filterValue.length)))) {
+        if (filterValue[0] === "!") {
+            if (filterValue.substring(1, 13) === "Description:") {
+                if (!(items[i]['Description'].includes(filterValue.substring(13, filterValue.length)))) {
+                    result.push(items[i])
+                }
+            }
+            else if (!(items[i]['Description'].includes(filterValue.substring(1, filterValue.length)) || items[i]['Title'].includes(filterValue.substring(1, filterValue.length)))) {
+                result.push(items[i])
+            }
+        } else if (filterValue.substring(0, 12) === "Description:" && items[i]['Description'].includes(filterValue.substring(12, filterValue.length))) {
+            result.push(items[i])
+        } else if (items[i]['Title'].includes(filterValue) || items[i]['Description'].includes(filterValue)) {
             result.push(items[i])
         }
-        else if (items[i]['Title'].includes(filterValue) || items[i]['Description'].includes(filterValue)) {
-            result.push(items[i])
-        }
-
     }
 
     return result
